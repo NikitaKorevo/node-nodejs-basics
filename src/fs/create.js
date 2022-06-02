bca58517316ad21fe2b6af14ed5487b411f13865
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-02 16:29:26 +0300

    feat: solve create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { open } from 'fs/promises';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+
 export const create = async () => {
   try {
+    const file = await open(path.join(__dirname, 'files', 'fresh.txt'), 'wx');
+
+    await file.write('I am fresh and young');
+    await file.close();
   } catch (error) {
     throw new Error('FS operation failed');
   }
