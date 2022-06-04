35d472a4293f04dd5a631e00dfee50f61b0d6648
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-04 21:40:08 +0300

    feat: solve streams/read.js subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,15 @@
+import { join } from 'path';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
+import { stdout } from 'process';
+import { createReadStream } from 'fs';
+
+const { __dirname } = useFilenameDirname(import.meta.url);
+
 export const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
+
+  stream.on('data', (chunk) => stdout.write(chunk));
+  stream.on('error', (error) => console.log(error.message));
 };
+
+read();
