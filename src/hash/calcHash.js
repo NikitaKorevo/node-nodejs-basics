c927d0ec72cd262960c4f283ac323ea804ebd12b
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-04 13:25:31 +0300

    feat: solve calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import { join } from 'path';
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
+
+const { __dirname } = useFilenameDirname(import.meta.url);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  return readFile(join(__dirname, 'files', 'fileToCalculateHashFor.txt'))
+    .then((data) => {
+      return createHash('sha256').update(data).digest('hex');
+    })
+    .catch(() => {
+      console.log(new Error('FS operation failed'));
+    });
 };
+
+console.log(await calculateHash());
