955b8068f5d791e5c192c5444f2bb0db0b3aa82f
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-04 22:26:19 +0300

    feat: solve streams/write.js subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,15 +1,15 @@
 import { join } from 'path';
-import useFilenameDirname from '../utils/useFilenameDirname.mjs';
-import { stdout } from 'process';
 import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
 
 const { __dirname } = useFilenameDirname(import.meta.url);
 
 export const read = async () => {
-  const stream = createReadStream(join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
+  const readStream = createReadStream(join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
 
-  stream.on('data', (chunk) => stdout.write(chunk));
-  stream.on('error', (error) => console.log(error.message));
+  readStream.on('data', (chunk) => stdout.write(chunk));
+  readStream.on('error', (error) => console.log(error.message));
 };
 
 read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
+
+const { __dirname } = useFilenameDirname(import.meta.url);
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(join(__dirname, 'files', 'fileToWrite.txt'));
+
+  writeStream.on('error', (error) => console.log(error));
+  stdin.on('data', (chunk) => writeStream.write(chunk));
 };
+
+write();
