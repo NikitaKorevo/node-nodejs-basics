cc9009781a1b973fc0d18918216c177188e2678f
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-05 00:46:58 +0300

    feat: solve zip/decompress.js subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,19 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createUnzip } from 'zlib';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
+
+const { __dirname } = useFilenameDirname(import.meta.url);
+
 export const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(join(__dirname, 'files', 'archive.gz'));
+  const writeStream = createWriteStream(join(__dirname, 'files', 'fileToCompress.txt'));
+  const gunzip = createUnzip();
+
+  await pipeline(readStream, gunzip, writeStream).catch((error) => {
+    console.log(error.message);
+  });
 };
+
+decompress();
