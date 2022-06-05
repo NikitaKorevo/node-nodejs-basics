db1b13c34e6265fbc210a2a75f435f104eed21ac
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-06-05 18:28:14 +0300

    feat: solve wt/worker subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,43 @@
+import { join } from 'path';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import useFilenameDirname from '../utils/useFilenameDirname.mjs';
+
+const { __dirname } = useFilenameDirname(import.meta.url);
+
 export const performCalculations = async () => {
-    // Write your code here
+  const incrementalNumber = 10;
+  const amountCPU = cpus().length;
+  const workers = [];
+
+  for (let i = incrementalNumber; i < incrementalNumber + amountCPU; i++) {
+    const workerPromise = new Promise((resolve, reject) => {
+      const worker = new Worker(join(__dirname, 'worker.js'), {
+        workerData: i,
+      });
+
+      worker.on('message', (result) => resolve(result));
+      worker.on('error', () => reject(null));
+    });
+
+    workers.push(workerPromise);
+  }
+
+  return await Promise.allSettled(workers).then((workerResults) => {
+    return workerResults.map(({ status, value }) => {
+      if (status === 'fulfilled') {
+        return {
+          status: 'resolved',
+          data: value,
+        };
+      } else {
+        return {
+          status: 'error',
+          data: reason,
+        };
+      }
+    });
+  });
 };
+
+console.log(await performCalculations());
